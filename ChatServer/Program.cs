b6681089d287ef2e54ffaf99fd82049110a51dd1
s87033tw <s87033tw@gmail.com>
2020-05-06 19:56:21 +0800

    rename

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -4,7 +4,7 @@ using System.Net.Sockets;
 using System.Threading;
 using System.IO;
 
-namespace p2pServer
+namespace ChatServer
 {
     class Program
     {
@@ -91,3 +91,4 @@ namespace p2pServer
         }
     }
 }
+
--- ChatServer/obj/Debug/netcoreapp3.1/ChatServer.AssemblyInfo.cs
+++ ChatServer/obj/Debug/netcoreapp3.1/ChatServer.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("p2pServer")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("p2pServer")]
-[assembly: System.Reflection.AssemblyTitleAttribute("p2pServer")]
+[assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 類別產生。
