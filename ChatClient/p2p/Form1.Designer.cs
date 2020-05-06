186d7df0e0d1b78ab6521ae8b5e4bf51a9580200
s87033tw <s87033tw@gmail.com>
2020-05-06 20:12:16 +0800

    add client_UI

---
+++ ChatClient/p2p/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 325);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(500, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(465, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Enabled = false;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox2.Location = new System.Drawing.Point(40, 19);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(500, 300);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ChatClient/p2p/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        TcpListener tcpL;
+        int port = 8000;
+        IPEndPoint ipep;
+        Socket server;
+        bool isConnected = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client();
+            textBox2.AppendText("已連線\r\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tcpL.Data(textBox1.Text);
+
+            if (textBox1.Text.Equals("esc"))
+            {
+                MessageBox.Show("連線已關閉");
+                isConnected = false;
+                server.Close();
+            }
+        }
+
+        private void client()
+        {
+            while (true)
+                try
+                {
+                    ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    MessageBox.Show(port.ToString());
+                    server.Connect(ipep);
+                    isConnected = true;
+                    tcpL = new TcpListener(server);
+                    break;
+                }
+                catch (SocketException e)
+                {
+                    port++;
+                }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                tcpL.Data("esc"); 
+                server.Close();
+            }
+
+            //server.Shutdown(SocketShutdown.Both);
+
+            if(MessageBox.Show("是否關閉程式", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Application.Exit();
+            //Environment.Exit(Environment.ExitCode);
+        }
+
+    }
+
+    public class TcpListener
+    {
+        Socket socket;
+        Thread outThread;
+        NetworkStream stream;
+        StreamWriter writer;
+        string data = "";
+        static string text = "0";
+        bool input = false;
+
+
+        public TcpListener(Socket s)
+        {
+            socket = s;
+            stream = new NetworkStream(s);
+            writer = new StreamWriter(stream);
+            outThread = new Thread(new ThreadStart(run));
+            outThread.Start();
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+
+                if (input)
+                {
+                    data = text;
+                    writer.WriteLine(data);
+                    writer.Flush();
+                    input = false;
+                    Thread.Sleep(10);
+                }
+            }
+        }
+
+        public void Data(string stream)
+        {
+            text = stream;
+            input = true;
+        }
+    }
+}
